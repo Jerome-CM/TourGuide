68282ec0e8c7846367ab76b8887402880a66731b
Bouteveille jerome <formation@community-technologie.fr>
2023-03-06 21:43:51 +0100

    Add lombok

---
+++ TourGuide/src/main/java/tourGuide/dto/NearbyAttractionsDTO.java
@@ -0,0 +1,19 @@
+package tourGuide.dto;
+
+import java.util.HashMap;
+
+public class nearbyAttractions {
+
+    String tourtistAttractionName;
+    HashMap<String, String> attractionLocation; //lat/long,, distance, reward
+    HashMap<String, String> userLocation; //lat/long,
+
+// Les points de récompense d'attraction peuvent être collectés auprès de RewardsCentral
+
+
+    /*Name of Tourist attraction,
+     Tourist attractions lat/long,
+     The user's location lat/long,
+     The distance in miles between the user's location and each of the attractions.
+     The reward points for visiting each Attraction.*/
+}
