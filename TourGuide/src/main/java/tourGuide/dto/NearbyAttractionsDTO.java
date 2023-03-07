34078710fd4ede6928fc35f1c8a2ba64be8eef32
Bouteveille jerome <formation@community-technologie.fr>
2023-03-07 16:43:16 +0100

    Update DTO

--- TourGuide/src/main/java/tourGuide/dto/NearbyAttractionsDTO.java
+++ TourGuide/src/main/java/tourGuide/dto/NearbyAttractionsDTO.java
@@ -1,25 +1,26 @@
 package tourGuide.dto;
 
+import gpsUtil.location.Location;
+import gpsUtil.location.VisitedLocation;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.HashMap;
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NearbyAttractionsDTO {
 
     private String tourtistAttractionName;
-    private HashMap<String, String> attractionLocation; //lat/long, distance, reward
-    private HashMap<String, String> userLocation;
+    private Location attractionLocation;
+
+    private Location userLocation;
 
     private double distanceInMiles;
 
-// Les points de récompense d'attraction peuvent être collectés auprès de RewardsCentral
+    private int reward;
 
-
-    /*Name of Tourist attraction,
-     Tourist attractions lat/long,
-     The user's location lat/long,
-     The distance in miles between the user's location and each of the attractions.
-     The reward points for visiting each Attraction.*/
 }
--- TourGuide/src/main/java/tourGuide/dto/UserLocationListDTO.java
+++ TourGuide/src/main/java/tourGuide/dto/UserLocationListDTO.java
@@ -1,2 +1,22 @@
-package tourGuide.dto;public class UserLocationListDTO {
+package tourGuide.dto;
+
+import com.jsoniter.output.JsonStream;
+import gpsUtil.location.Location;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class UserLocationListDTO {
+
+    private Map<String, Location> locationMap = new HashMap<>();
+
+    public void add(String userId, Location location){
+        locationMap.put(userId, location);
+    }
+
+    @Override
+    public String toString(){
+        return JsonStream.serialize(locationMap);
+    }
 }
